d9509b781aa684430b00041a087fd78012e2261e
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-13 06:56:07 +0100

    Cooking...

--- Index.js
+++ Index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
-const newspapers = require("./Utilities/Index")
-console.log(newspapers)
 
 const app = express()
 const port = 8888
 
-app.get('/', (req, res) => res.send('Heyy There, I am a cat.'))
+const newspapers = require("./Utilities/Index")
+
+const articles = []
+
+app.get('/', (req, res) => {
+    res.json('This is a Climate Change News API')
+})
+
+app.get('/News', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                articles.push({ title, url })
+            })
+            res.json(articles)
+        }).catch((err) => {
+            console.error(err)
+        });
+})
+
 app.listen(port, () => console.log(`Project is running on port ${port}!`))
