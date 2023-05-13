9db90447eeb77f4703c13e542d3783e4111d4f53
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-13 08:12:19 +0100

    Cooking...

--- Utilities/Index.js
+++ Utilities/Index.js
@@ -7,27 +7,27 @@ module.exports = [
     {
         name: 'news medical life sciences',
         address: 'https://www.news-medical.net/news/20221027/New-report-highlights-the-heat-impacts-of-air-pollution-in-Africa.aspx#:~:text=Africa%20is%20home%20to%20five,term%20exposure%20to%20air%20pollution.',
-        base: ''
+        base: 'https://www.news-medical.net'
     },
     {
         name: 'unep',
         address: 'https://www.unep.org/resources/report/air-pollution-and-development-africa-impacts-health-economy-and-human-capital',
-        base: '',
+        base: 'https://www.unep.org',
     },
     {
         name: 'unep.org',
         address: 'https://www.unep.org/explore-topics/disasters-conflicts/where-we-work/nigeria/ogonilands-oil-history',
-        base: '',
+        base: 'https://www.unep.org',
     },
     {
         name: 'cnn',
         address: 'https://edition.cnn.com/2022/05/25/africa/shell-oil-spills-nigeria-intl-cmd/index.html',
-        base: '',
+        base: 'https://edition.cnn.com/2022',
     },
     {
         name: 'africanews',
         address: 'https://www.africanews.com/2022/10/28/africa-air-pollution-a-silent-killer-in-cities-study//',
-        base: '',
+        base: 'https://www.africanews.com',
     },
     {
         name: 'sei',
@@ -47,6 +47,6 @@ module.exports = [
     {
         name: 'france24',
         address: 'https://www.france24.com/en/tv-shows/revisited/20210702-polluted-by-the-oil-industry-life-in-nigeria-s-ogoniland',
-        base: '',
+        base: 'https://www.france24.com',
     },
 ]
