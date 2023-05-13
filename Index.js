02bb541afc1fbd675353504336e3335369877550
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-13 06:27:03 +0100

    Cooking...

--- Index.js
+++ Index.js
@@ -1,6 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const newspapers = require("./Utilities/Index")
+console.log(newspapers)
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express()
+const port = 8888
+
+app.get('/', (req, res) => res.send('Heyy There, I am a cat.'))
+app.listen(port, () => console.log(`Project is running on port ${port}!`))
