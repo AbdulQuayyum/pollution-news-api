4e7244a9c0832371df746832c052039e5282f60b
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-05-13 07:51:56 +0100

    Cooking...

--- Index.js
+++ Index.js
@@ -9,27 +9,33 @@ const newspapers = require("./Utilities/Index")
 
 const articles = []
 
-app.get('/', (req, res) => {
-    res.json('This is a Climate Change News API')
-})
-
-const link = ""
-
-app.get('/News', (req, res) => {
-    axios.get(link)
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
         .then((response) => {
             const html = response.data
             const $ = cheerio.load(html)
 
-            $('a:contains("pollution")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-                articles.push({ title, url })
-            })
-            res.json(articles)
-        }).catch((err) => {
+            $('a:contains("pollution")', html)
+                .each(function () {
+                    const title = $(this).text()
+                    const url = $(this).attr('href')
+                    articles.push({
+                        title,
+                        url: newspaper?.base + url,
+                        source: newspaper?.name
+                    })
+                })
+
+        })
+        .catch((err) => {
             console.error(err)
         });
 })
+
+app.get('/', (req, res) => {
+    res.json('This is a Climate Change News API')
+})
+
+app.get('/News', (req, res) => { res.json(articles) })
 
 app.listen(port, () => console.log(`Project is running on port ${port}!`))
